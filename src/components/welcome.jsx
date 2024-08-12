64ba0edfe13e6862d710321fffa0840e2b8c1ea5
Alfredo Montoya <alfredo.hernandez@catolica.edu.sv>
2024-08-11 23:08:51 -0600

    Avances en el welcome

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { CheckIcon } from '@heroicons/react/20/solid';
+import { CalendarDaysIcon, HandRaisedIcon } from '@heroicons/react/24/outline';
 
 const includedFeatures = [
   'Acceso Anticipado',
@@ -52,6 +53,31 @@ const animationStyle = `
 const Welcome = React.memo(() => {
   const [activeNav, setActiveNav] = useState('Bienvenida');
 
+  useEffect(() => {
+    const handleScroll = () => {
+      const sections = document.querySelectorAll('section[id]');
+      const scrollY = window.scrollY;
+
+      let currentSection = '';
+
+      sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+
+        if (scrollY >= sectionTop - 60 && scrollY < sectionTop + sectionHeight - 60) {
+          currentSection = section.getAttribute('id');
+        }
+      });
+
+      setActiveNav(currentSection);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   const handleNavClick = (id) => {
     document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' });
     setActiveNav(id);
@@ -65,7 +91,7 @@ const Welcome = React.memo(() => {
         <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
-            <img src="/E-Sync/LG-E-SYNC.png" className="h-12 w-auto" alt="E-SYNC Logo" />
+              <img src="/E-Sync/LG-E-SYNC.png" className="h-12 w-auto" alt="E-SYNC Logo" />
             </a>
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
@@ -185,7 +211,7 @@ const Welcome = React.memo(() => {
                       href="#"
                       className="mt-10 block w-full rounded-md bg-custom-yellow px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-custom-yellow-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     >
-                      Get access
+                      Obtener Ahora
                     </a>
                     <p className="mt-6 text-xs leading-5 text-gray-600">
                       Facturas y recibos disponibles para facilitar el reembolso de la empresa
@@ -200,6 +226,96 @@ const Welcome = React.memo(() => {
       {/* Fin Subscripciones */}
 
       {/* Soporte */}
+      <section id="soporte">
+        <div className="relative isolate overflow-hidden py-16 sm:py-24 lg:py-32">
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:max-w-none lg:grid-cols-2">
+              <div className="max-w-xl lg:max-w-lg">
+                <h2 className="text-3xl font-bold tracking-tight text-black sm:text-4xl">¿Cómo podemos ayudarte?</h2>
+                <p className="mt-4 text-lg leading-8 text-black text-justify">
+                  En <span className='text-custom-blue font-bold'>E-Sync</span>, nos dedicamos a ofrecerte una experiencia de entretenimiento excepcional, y eso incluye un soporte al 
+                  cliente de primera calidad. Te enviaremos un correo al que nos brindaste en breve, asegurándonos de responder a tu consulta lo más pronto posible. Estamos aquí para 
+                  ayudarte en cada paso del camino, porque tu satisfacción es nuestra máxima prioridad.
+                </p>
+                <div className="mt-6 flex max-w-md gap-x-4">
+                  <label htmlFor="email-address" className="sr-only">
+                    Email address
+                  </label>
+                  <input
+                    id="email-address"
+                    name="email"
+                    type="email"
+                    required
+                    placeholder="Correo Electronico"
+                    autoComplete="email"
+                    className="min-w-0 flex-auto rounded-md border-0 /5 px-3.5 py-2 text-black shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  />
+                  <button
+                    type="submit"
+                    className="flex-none rounded-md bg-custom-yellow px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm hover:bg-custom-yellow-dark focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                  >
+                    Enviar
+                  </button>
+                </div>
+              </div>
+              <dl className="grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2 lg:pt-2">
+                <div className="flex flex-col items-start">
+                  <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                    <CalendarDaysIcon aria-hidden="true" className="h-6 w-6 text-custom-blue" />
+                  </div>
+                  <dt className="mt-4 font-semibold text-custom-red">Articulos Semanales</dt>
+                  <dd className="mt-2 leading-7 text-black text-justify">
+                    Además de nuestro soporte, recibirás artículos semanales directamente en tu correo. Estos artículos están diseñados para mantenerte informado sobre los últimos eventos, 
+                    tendencias y consejos exclusivos que harán que tu experiencia en E-Sync sea aún mejor.
+                  </dd>
+                </div>
+                <div className="flex flex-col items-start">
+                  <div className="rounded-md bg-white/5 p-2 ring-1 ring-white/10">
+                    <HandRaisedIcon aria-hidden="true" className="h-6 w-6 text-custom-blue" />
+                  </div>
+                  <dt className="mt-4 font-semibold text-custom-red">No spam</dt>
+                  <dd className="mt-2 leading-7 text-black text-justify">
+                    Valoramos tu privacidad, por lo que puedes estar seguro de que solo recibirás correos relevantes. En E-Sync, no toleramos el spam; solo te enviaremos información útil y actualizaciones importantes relacionadas con tu experiencia.
+                  </dd>
+                </div>
+              </dl>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Fin Soporte */}
+
+      {/* Acerca De */}
+      <section id="acerca-de">
+        <div className="relative isolate overflow-hidden py-24 sm:py-32">
+          <div
+            aria-hidden="true"
+            className="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+          >
+          </div>
+          <div className="mx-auto max-w-7xl px-6 lg:px-8">
+            <div className="flex flex-col lg:flex-row items-center lg:items-start">
+              <div className="flex-shrink-0 lg:w-1/2">
+                <img 
+                  src="/E-Sync/LG-E-SYNC.png" 
+                  alt="Descripción de la imagen" 
+                  className="w-full h-auto object-cover rounded-lg"
+                />
+              </div>
+              <div className="mt-10 lg:mt-0 lg:ml-8 lg:w-1/2">
+                <h2 className="text-4xl font-bold tracking-tight text-black sm:text-6xl">Acerca De <span className='text-custom-blue'>E-Sync</span></h2>
+                <p className="mt-6 text-lg leading-8 text-black text-justify">
+                  <span className='text-custom-blue font-bold'>E-Sync</span> es tu destino definitivo para el entretenimiento en vivo, ofreciendo una plataforma innovadora para la compra de boletos para los eventos más emocionantes del momento. en <span className='text-custom-blue font-bold'>E-Sync</span> tenemos algo para cada amante del espectáculo.
+                  Fundada con la misión de transformar la forma en que experimentas el entretenimiento en vivo, <span className='text-custom-blue font-bold'>E-Sync</span> se dedica a brindarte una experiencia sin igual. Nuestra plataforma está diseñada para ofrecerte acceso rápido y seguro a los eventos que más te apasionan, con beneficios exclusivos para nuestros miembros a través del <span className='text-custom-red font-bold'>E-Pass</span>.
+                  Nuestro compromiso es asegurar que cada compra sea sencilla, cada evento memorable, y cada experiencia, excepcional. Además, nuestro equipo de soporte al cliente está siempre listo para ayudarte en cada paso del camino, garantizando tu total satisfacción.
+                  En <span className='text-custom-blue font-bold'>E-Sync</span>, no solo vendemos boletos; te conectamos con el espectáculo, la emoción y la magia del entretenimiento en vivo. Únete a nosotros y descubre una nueva forma de disfrutar tus eventos favoritos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      {/* Fin Acerca De */}
     </>
   );
 });
